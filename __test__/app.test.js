aeaa55df436197d3db52c0a43a40f220addb135d
Dr. Ahmad Jamal <ahmad@tulas.edu.in>
2023-11-12 22:19:55 +0530

    TEST-User Registration: Sign up for a new account

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,10 +1,43 @@
-const app = require('../app');
-const request = require('supertest');
+// NOTE Server - client architecture testing setup
+const request = require("supertest");
+// NOTE Used to parse markup language
+const cheerio = require("cheerio");
 
-// sample test for cheking jest is working or not. 
-test('GET / route returns status 200', async () => {
-    const response = await request(app).get('/');
-  
-    expect(response.status).toBe(200);
+const db = require("../models/index");
+const app = require("../app");
+
+let server, agent;
+
+function extractTokenOfCSRF(res) {
+  let $ = cheerio.load(res.text);
+  return $("[name=_csrf]").val();
+}
+
+describe("User Test Suite", () => {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
+    server = app.listen(3005, () => {});
+    testAgent = request.agent(server);
+    await testAgent.get("/");
   });
-  
+
+  afterAll(async () => {
+    await db.sequelize.close();
+    server.close();
+  });
+
+  test("User Registration: Sign up for a new account", async () => {
+    let signupResponse = await testAgent.get("/signup");
+    const csrfToken = extractTokenOfCSRF(signupResponse);
+
+    let registrationResponse = await testAgent.post("/users").send({
+      firstName: "Ahmad",
+      lastName: "Jamal",
+      email: "ahmadjamalcs@gmail.com",
+      password: "plmnko",
+      _csrf: csrfToken,
+    });
+
+    expect(registrationResponse.statusCode).toBe(302);
+  });
+});
