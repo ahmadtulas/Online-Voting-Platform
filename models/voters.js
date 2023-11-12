c9e55e80ad12dd546ab08838e9c9decf0b13ae53
Dr. Ahmad Jamal <ahmad@tulas.edu.in>
2023-11-12 14:15:10 +0530

    Dashboard Table content change

--- models/voters.js
+++ models/voters.js
@@ -1,46 +1,46 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Voters extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      Voters.belongsTo(models.Elections, {
-        foreignKey: "electionId",
-      });
-      Voters.hasMany(models.Votes, {
-        foreignKey: "voterId",
-      });
-    }
-    static addVoter(voterId, password, electionId) {
-      return this.create({
-        voterId,
-        password,
-        electionId,
-      });
-    }
-    static async delete(electionId,id) {
-      return this.destroy({
-        where: {
-          id,
+  'use strict';
+  const {
+    Model
+  } = require('sequelize');
+  module.exports = (sequelize, DataTypes) => {
+    class Voters extends Model {
+      /**
+       * Helper method for defining associations.
+       * This method is not a part of Sequelize lifecycle.
+       * The `models/index` file will call this method automatically.
+       */
+      static associate(models) {
+        // define association here
+        Voters.belongsTo(models.Elections, {
+          foreignKey: "electionId",
+        });
+        Voters.hasMany(models.Votes, {
+          foreignKey: "voterId",
+        });
+      }
+      static addVoter(voterId, password, electionId) {
+        return this.create({
+          voterId,
+          password,
           electionId,
-        },
-      });
+        });
+      }
+      static async delete(electionId,id) {
+        return this.destroy({
+          where: {
+            id,
+            electionId,
+          },
+        });
+      }
     }
-  }
-  
-  Voters.init({
-    voterId: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Voters',
-  });
-  return Voters;
-};
+    
+    Voters.init({
+      voterId: DataTypes.STRING,
+      password: DataTypes.STRING
+    }, {
+      sequelize,
+      modelName: 'Voters',
+    });
+    return Voters;
+  };
